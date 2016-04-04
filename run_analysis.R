907771b34cf19f57434941bda28422e347020712
Safkat Chowdhury <safkat.93@gmail.com>
2016-04-04 06:13:07 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ step 1
 
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+download.file(fileUrl,destfile="./data/Dataset.zip")
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
 ## unzip
